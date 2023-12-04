59974e086f9557385a2a2489c75e72d32e73139c
Jason Ruesch <jruesch@degreed.com>
2023-12-04 14:49:58 -0500

    chore(repo): add CODEOWNERS file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   '{apps,libs}/**/*.{ts,tsx,js,jsx,html,json,scss,css,md}': [
     'nx affected:lint --uncommitted --fix true',
-    // 'nx affected:test --uncommitted',
   ],
   '*.{ts,tsx,js,jsx,html,json,scss,css,md,yaml,yml}': [
     'nx format:write --base=main',
