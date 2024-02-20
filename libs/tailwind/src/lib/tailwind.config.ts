25e6bfaf0b1777933a6d2e439227a443558b9379
Jason Ruesch <jruesch@degreed.com>
2024-02-19 22:33:52 -0500

    feat(tailwind): set test color to blue

--- libs/tailwind/src/lib/tailwind.config.ts
+++ libs/tailwind/src/lib/tailwind.config.ts
@@ -18,7 +18,7 @@ export const tailwindPreset = {
       transparent: colors.transparent,
       black: colors.black,
       white: colors.white,
-      test: 'black',
+      test: 'blue',
     },
     extend: {
       lineHeight: {
