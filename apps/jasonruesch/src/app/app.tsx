0591b4906b1558ae57300050490f4a064f5d2d30
Jason Ruesch <jruesch@degreed.com>
2024-02-21 16:55:49 -0500

    feat(jasonruesch): set test color to blue

--- apps/jasonruesch/src/app/app.tsx
+++ apps/jasonruesch/src/app/app.tsx
@@ -7,7 +7,7 @@ export function App() {
       <Layout />
       <ScrollRestoration />
 
-      <div className="hidden text-test">Test color: gray</div>
+      <div className="hidden text-test">Test color: blue</div>
     </>
   );
 }
