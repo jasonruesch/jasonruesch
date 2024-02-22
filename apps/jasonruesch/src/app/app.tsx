e2bcf811f6cb4b72102e5fb2d51cd40b21791b53
Jason Ruesch <jruesch@degreed.com>
2024-02-21 21:04:52 -0500

    fix(jasonruesch): increment test color label

--- apps/jasonruesch/src/app/app.tsx
+++ apps/jasonruesch/src/app/app.tsx
@@ -7,7 +7,7 @@ export function App() {
       <Layout />
       <ScrollRestoration />
 
-      <div className="hidden text-test">Test color: blue (4)</div>
+      <div className="hidden text-test">Test color: blue (5)</div>
     </>
   );
 }
