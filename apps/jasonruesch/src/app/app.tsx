10571d94cc65fa58e8bdb17d2965e17a8cbf9d93
Jason Ruesch <jruesch@degreed.com>
2024-02-23 21:27:12 -0500

    feat(jasonruesch): set test color to pink

--- apps/jasonruesch/src/app/app.tsx
+++ apps/jasonruesch/src/app/app.tsx
@@ -7,7 +7,7 @@ export function App() {
       <Layout />
       <ScrollRestoration />
 
-      <div className="hidden text-test">Test color: purple (2)</div>
+      <div className="hidden text-test">Test color: pink</div>
     </>
   );
 }
