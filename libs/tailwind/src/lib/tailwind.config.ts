0980740749da8d2fac2a4befd0f6d42e64fea71e
Jason Ruesch <jruesch@degreed.com>
2024-03-02 23:41:57 -0500

    feat(tailwind): release process test

--- libs/tailwind/src/lib/tailwind.config.ts
+++ libs/tailwind/src/lib/tailwind.config.ts
@@ -19,6 +19,7 @@ export const tailwindPreset = {
       transparent: colors.transparent,
       black: colors.black,
       white: colors.white,
+      test: 'blue',
     },
     extend: {
       lineHeight: {
