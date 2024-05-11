32c3f7f152f22066932b7c268a226f71c1c6cedd
Jason Ruesch <jruesch@degreed.com>
2024-05-11 13:52:13 -0400

    feat(jasonruesch): add React Router

--- apps/jasonruesch/src/app/app.tsx
+++ apps/jasonruesch/src/app/app.tsx
@@ -1,9 +1,18 @@
-import NxWelcome from './nx-welcome';
+import { Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="Jason Ruesch" />
+    <div className="min-h-dvh grid place-content-center p-4">
+      <Routes>
+        <Route
+          path="/"
+          element={<h1 className="text-4xl font-bold">Jason Ruesch</h1>}
+        />
+        <Route
+          path="/about"
+          element={<h1 className="text-4xl font-bold">About</h1>}
+        />
+      </Routes>
     </div>
   );
 }
