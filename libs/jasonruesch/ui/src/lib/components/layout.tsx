880b539f987fd84ab247c9ad8c59ee5afb01bb33
Jason Ruesch <jruesch@degreed.com>
2024-07-26 16:31:56 -0400

    feat(jasonruesch): add skip navigation link

--- libs/jasonruesch/ui/src/lib/components/layout.tsx
+++ libs/jasonruesch/ui/src/lib/components/layout.tsx
@@ -1,18 +1,30 @@
+import { useLocation } from 'react-router-dom';
+
 import { EasterEggLink } from './easter-egg-link';
 import { Header } from './header';
+
+import styles from './layout.module.css';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps) => {
+  const { pathname } = useLocation();
+
   return (
     <>
       <div id="background"></div>
 
+      <div id="skip" className={styles.skip}>
+        <a href={`${pathname}#content`} className="z-30">
+          Skip Content
+        </a>
+      </div>
+
       <Header />
 
-      <main>
+      <main id="content">
         {/* The relative position is needed to work with the background component's fixed or absolute position */}
         <div className="relative">{children}</div>
 
