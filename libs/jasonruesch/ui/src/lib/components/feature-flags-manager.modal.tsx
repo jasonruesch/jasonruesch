9202d001d8edba8f895fc40cac3e304da499faa9
Jason Ruesch <jruesch@degreed.com>
2024-07-27 20:54:43 -0400

    feat(jasonruesch): improve modal

--- libs/jasonruesch/ui/src/lib/components/feature-flags-manager.modal.tsx
+++ libs/jasonruesch/ui/src/lib/components/feature-flags-manager.modal.tsx
@@ -49,7 +49,7 @@ export default function FeatureFlagsManager() {
           )}
         />
 
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto pt-20 px-4 pb-4 sm:px-6 sm:pb-6 md:pt-32 md:px-20 md:pb-20">
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto pt-20 px-4 pb-4 lg:pt-32 lg:px-20 lg:pb-20">
           <DialogPanel
             transition
             className="relative mx-auto w-full max-w-lg transform overflow-hidden rounded-lg bg-white pt-5 pb-4 text-left shadow-xl transition-all data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:duration-300 data-[enter]:ease-out data-[leave]:duration-200 data-[leave]:ease-in dark:bg-neutral-950"
@@ -81,7 +81,7 @@ export default function FeatureFlagsManager() {
               </div>
             </div>
 
-            <div className="mt-2 max-h-96 transform-gpu scroll-py-3 space-y-2 overflow-y-auto py-3 px-4 sm:ml-14">
+            <div className="mt-2 max-h-40 transform-gpu scroll-py-3 space-y-2 overflow-y-auto py-3 px-4 sm:ml-14 lg:max-h-96">
               {flags.map((flag) => (
                 <Field
                   key={flag.key}
