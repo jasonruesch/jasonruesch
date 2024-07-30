df1a5f42a05ecc592088b368d8a0dd00b7c5ba32
Jason Ruesch <jruesch@degreed.com>
2024-07-29 20:48:42 -0400

    feat(jasonruesch): add login page

--- libs/jasonruesch/ui/src/lib/pages/index.ts
+++ libs/jasonruesch/ui/src/lib/pages/index.ts
@@ -5,6 +5,7 @@ export * from './changelog/changelog';
 export * from './contact/contact';
 export * from './easter-egg/easter-egg';
 export * from './home/home';
+export * from './login/login';
 export * from './privacy/privacy';
 export * from './projects/projects';
 export * from './transparent/transparent';
