c4a5ee4475389d174dce8bccea080f8c7851eadf
Jason Ruesch <jruesch@redroverk12.com>
2025-06-25 23:20:22 -0400

    feat(jasonruesch): add spacing between nav items

--- apps/jasonruesch/app/app-nav.tsx
+++ apps/jasonruesch/app/app-nav.tsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router';
 
 export function AppNav() {
   return (
-    <nav>
+    <nav className="flex items-center gap-2">
       <NavLink to="/" end>
         Home
       </NavLink>
