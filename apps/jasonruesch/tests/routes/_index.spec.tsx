626895782424921f18ae2b6267725153d3d7a1dd
Jason Ruesch <jruesch@redroverk12.com>
2025-06-26 16:25:57 -0400

    test(jasonruesch): fix home route test

--- apps/jasonruesch/tests/routes/_index.spec.tsx
+++ apps/jasonruesch/tests/routes/_index.spec.tsx
@@ -1,16 +1,18 @@
-import { createRoutesStub } from 'react-router';
 import { render, screen, waitFor } from '@testing-library/react';
-import App from '../../app/app';
+import { createRoutesStub } from 'react-router';
+import { test } from 'vitest';
+
+import Home from '../../app/routes/home';
 
 test('renders loader data', async () => {
   const ReactRouterStub = createRoutesStub([
     {
       path: '/',
-      Component: App,
+      Component: Home,
     },
   ]);
 
   render(<ReactRouterStub />);
 
-  await waitFor(() => screen.findByText('Hello there,'));
+  await waitFor(() => screen.findByText('Software developer and designer.'));
 });
