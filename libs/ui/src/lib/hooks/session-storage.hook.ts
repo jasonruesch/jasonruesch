5849a0f72862e6131be09642fef44c09897f2bbe
Jason Ruesch <jruesch@redroverk12.com>
2025-06-27 12:44:57 -0400

    feat(ui): add splash screen component

---
+++ libs/ui/src/lib/hooks/session-storage.hook.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export const useSessionStorage = (key: string, initialValue: boolean) => {
+  const [value, setValue] = useState(() => {
+    try {
+      const item = window.sessionStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setItem = (newValue: boolean) => {
+    try {
+      setValue(newValue);
+      window.sessionStorage.setItem(key, JSON.stringify(newValue));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [value, setItem] as const;
+};
