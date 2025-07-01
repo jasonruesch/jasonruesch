9ace3adfeb1cc50fca7ffd2c7dfbbad31e2d9791
Jason Ruesch <jruesch@redroverk12.com>
2025-07-01 14:29:35 -0400

    feat(jasonruesch): minor update to the projects placeholder content

--- apps/jasonruesch/app/routes/projects.tsx
+++ apps/jasonruesch/app/routes/projects.tsx
@@ -72,12 +72,16 @@ export default function Projects() {
           ))}
         </ul>
       ) : (
-        <Card>
-          <Card.Title>Coming soon</Card.Title>
-          <Card.Description>
-            No projects to show yet. Check back soon!
-          </Card.Description>
-        </Card>
+        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+          <div className="flex max-w-3xl flex-col space-y-16">
+            <Card>
+              <Card.Title>Coming soon</Card.Title>
+              <Card.Description>
+                No projects to show yet. Check back soon!
+              </Card.Description>
+            </Card>
+          </div>
+        </div>
       )}
     </SimpleLayout>
   );
