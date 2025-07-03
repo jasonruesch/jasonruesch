94cfa27fff8ee63f4fa4dae05e6e8aff63c44065
Jason Ruesch <jruesch@redroverk12.com>
2025-07-02 20:54:53 -0400

    feat(jasonruesch): update footer responsiveness

--- libs/ui/src/lib/footer.tsx
+++ libs/ui/src/lib/footer.tsx
@@ -24,8 +24,8 @@ export function Footer({ navigation, version }: FooterProps) {
       <ContainerOuter>
         <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
           <ContainerInner>
-            <div className="flex flex-col items-center justify-between gap-6 md:flex-row">
-              <div className="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+            <div className="flex flex-col items-center justify-between gap-6 lg:flex-row">
+              <div className="flex flex-wrap gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 {navigation.map(({ to, name }) => (
                   <Link key={to} to={to}>
                     {name}
